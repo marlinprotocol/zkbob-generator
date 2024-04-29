2bd4fb4577d672cb14fc16ff792eefb2d41a7caf
Akshay Meher <akshay_111meher@hotmail.com>
2024-04-29 12:15:44 +0400

    update bindings

--- bindings/src/ecdsa.rs
+++ bindings/src/ecdsa.rs
@@ -16,7 +16,61 @@ pub mod ecdsa {
             constructor: ::core::option::Option::None,
             functions: ::std::collections::BTreeMap::new(),
             events: ::std::collections::BTreeMap::new(),
-            errors: ::std::collections::BTreeMap::new(),
+            errors: ::core::convert::From::from([
+                (
+                    ::std::borrow::ToOwned::to_owned("ECDSAInvalidSignature"),
+                    ::std::vec![
+                        ::ethers::core::abi::ethabi::AbiError {
+                            name: ::std::borrow::ToOwned::to_owned(
+                                "ECDSAInvalidSignature",
+                            ),
+                            inputs: ::std::vec![],
+                        },
+                    ],
+                ),
+                (
+                    ::std::borrow::ToOwned::to_owned("ECDSAInvalidSignatureLength"),
+                    ::std::vec![
+                        ::ethers::core::abi::ethabi::AbiError {
+                            name: ::std::borrow::ToOwned::to_owned(
+                                "ECDSAInvalidSignatureLength",
+                            ),
+                            inputs: ::std::vec![
+                                ::ethers::core::abi::ethabi::Param {
+                                    name: ::std::borrow::ToOwned::to_owned("length"),
+                                    kind: ::ethers::core::abi::ethabi::ParamType::Uint(
+                                        256usize,
+                                    ),
+                                    internal_type: ::core::option::Option::Some(
+                                        ::std::borrow::ToOwned::to_owned("uint256"),
+                                    ),
+                                },
+                            ],
+                        },
+                    ],
+                ),
+                (
+                    ::std::borrow::ToOwned::to_owned("ECDSAInvalidSignatureS"),
+                    ::std::vec![
+                        ::ethers::core::abi::ethabi::AbiError {
+                            name: ::std::borrow::ToOwned::to_owned(
+                                "ECDSAInvalidSignatureS",
+                            ),
+                            inputs: ::std::vec![
+                                ::ethers::core::abi::ethabi::Param {
+                                    name: ::std::borrow::ToOwned::to_owned("s"),
+                                    kind: ::ethers::core::abi::ethabi::ParamType::FixedBytes(
+                                        32usize,
+                                    ),
+                                    internal_type: ::core::option::Option::Some(
+                                        ::std::borrow::ToOwned::to_owned("bytes32"),
+                                    ),
+                                },
+                            ],
+                        },
+                    ],
+                ),
+            ]),
             receive: false,
             fallback: false,
         }
@@ -26,13 +80,13 @@ pub mod ecdsa {
         __abi,
     );
     #[rustfmt::skip]
-    const __BYTECODE: &[u8] = b"`V`7`\x0B\x82\x82\x829\x80Q`\0\x1A`s\x14`*WcNH{q`\xE0\x1B`\0R`\0`\x04R`$`\0\xFD[0`\0R`s\x81S\x82\x81\xF3\xFEs\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \x80\xF0UC\x93\xEC6.\xF1\xF4R\xA9\x94\xD8\xB9\xFB\xC4W\x83\x9E\x05}&g]\xF1y\x05\x83\x81tYdsolcC\0\x08\x14\x003";
+    const __BYTECODE: &[u8] = b"`V`7`\x0B\x82\x82\x829\x80Q`\0\x1A`s\x14`*WcNH{q`\xE0\x1B`\0R`\0`\x04R`$`\0\xFD[0`\0R`s\x81S\x82\x81\xF3\xFEs\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \xF7\xA7\xEB\x87\x8F=\xBD\xA4\x0F\xF7\xBF\xB3\x80\xAB\x94\xA5\xAAqy\x84\xDF\xFE>\x02\xF3D\xC7\xC7\xE3\xBCe\x16dsolcC\0\x08\x14\x003";
     /// The bytecode of the contract.
     pub static ECDSA_BYTECODE: ::ethers::core::types::Bytes = ::ethers::core::types::Bytes::from_static(
         __BYTECODE,
     );
     #[rustfmt::skip]
-    const __DEPLOYED_BYTECODE: &[u8] = b"s\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \x80\xF0UC\x93\xEC6.\xF1\xF4R\xA9\x94\xD8\xB9\xFB\xC4W\x83\x9E\x05}&g]\xF1y\x05\x83\x81tYdsolcC\0\x08\x14\x003";
+    const __DEPLOYED_BYTECODE: &[u8] = b"s\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \xF7\xA7\xEB\x87\x8F=\xBD\xA4\x0F\xF7\xBF\xB3\x80\xAB\x94\xA5\xAAqy\x84\xDF\xFE>\x02\xF3D\xC7\xC7\xE3\xBCe\x16dsolcC\0\x08\x14\x003";
     /// The deployed bytecode of the contract.
     pub static ECDSA_DEPLOYED_BYTECODE: ::ethers::core::types::Bytes = ::ethers::core::types::Bytes::from_static(
         __DEPLOYED_BYTECODE,
@@ -118,6 +172,177 @@ pub mod ecdsa {
     for ECDSA<M> {
         fn from(contract: ::ethers::contract::Contract<M>) -> Self {
             Self::new(contract.address(), contract.client())
+        }
+    }
+    ///Custom Error type `ECDSAInvalidSignature` with signature `ECDSAInvalidSignature()` and selector `0xf645eedf`
+    #[derive(
+        Clone,
+        ::ethers::contract::EthError,
+        ::ethers::contract::EthDisplay,
+        serde::Serialize,
+        serde::Deserialize,
+        Default,
+        Debug,
+        PartialEq,
+        Eq,
+        Hash
+    )]
+    #[etherror(name = "ECDSAInvalidSignature", abi = "ECDSAInvalidSignature()")]
+    pub struct ECDSAInvalidSignature;
+    ///Custom Error type `ECDSAInvalidSignatureLength` with signature `ECDSAInvalidSignatureLength(uint256)` and selector `0xfce698f7`
+    #[derive(
+        Clone,
+        ::ethers::contract::EthError,
+        ::ethers::contract::EthDisplay,
+        serde::Serialize,
+        serde::Deserialize,
+        Default,
+        Debug,
+        PartialEq,
+        Eq,
+        Hash
+    )]
+    #[etherror(
+        name = "ECDSAInvalidSignatureLength",
+        abi = "ECDSAInvalidSignatureLength(uint256)"
+    )]
+    pub struct ECDSAInvalidSignatureLength {
+        pub length: ::ethers::core::types::U256,
+    }
+    ///Custom Error type `ECDSAInvalidSignatureS` with signature `ECDSAInvalidSignatureS(bytes32)` and selector `0xd78bce0c`
+    #[derive(
+        Clone,
+        ::ethers::contract::EthError,
+        ::ethers::contract::EthDisplay,
+        serde::Serialize,
+        serde::Deserialize,
+        Default,
+        Debug,
+        PartialEq,
+        Eq,
+        Hash
+    )]
+    #[etherror(name = "ECDSAInvalidSignatureS", abi = "ECDSAInvalidSignatureS(bytes32)")]
+    pub struct ECDSAInvalidSignatureS {
+        pub s: [u8; 32],
+    }
+    ///Container type for all of the contract's custom errors
+    #[derive(
+        Clone,
+        ::ethers::contract::EthAbiType,
+        serde::Serialize,
+        serde::Deserialize,
+        Debug,
+        PartialEq,
+        Eq,
+        Hash
+    )]
+    pub enum ECDSAErrors {
+        ECDSAInvalidSignature(ECDSAInvalidSignature),
+        ECDSAInvalidSignatureLength(ECDSAInvalidSignatureLength),
+        ECDSAInvalidSignatureS(ECDSAInvalidSignatureS),
+        /// The standard solidity revert string, with selector
+        /// Error(string) -- 0x08c379a0
+        RevertString(::std::string::String),
+    }
+    impl ::ethers::core::abi::AbiDecode for ECDSAErrors {
+        fn decode(
+            data: impl AsRef<[u8]>,
+        ) -> ::core::result::Result<Self, ::ethers::core::abi::AbiError> {
+            let data = data.as_ref();
+            if let Ok(decoded) = <::std::string::String as ::ethers::core::abi::AbiDecode>::decode(
+                data,
+            ) {
+                return Ok(Self::RevertString(decoded));
+            }
+            if let Ok(decoded) = <ECDSAInvalidSignature as ::ethers::core::abi::AbiDecode>::decode(
+                data,
+            ) {
+                return Ok(Self::ECDSAInvalidSignature(decoded));
+            }
+            if let Ok(decoded) = <ECDSAInvalidSignatureLength as ::ethers::core::abi::AbiDecode>::decode(
+                data,
+            ) {
+                return Ok(Self::ECDSAInvalidSignatureLength(decoded));
+            }
+            if let Ok(decoded) = <ECDSAInvalidSignatureS as ::ethers::core::abi::AbiDecode>::decode(
+                data,
+            ) {
+                return Ok(Self::ECDSAInvalidSignatureS(decoded));
+            }
+            Err(::ethers::core::abi::Error::InvalidData.into())
+        }
+    }
+    impl ::ethers::core::abi::AbiEncode for ECDSAErrors {
+        fn encode(self) -> ::std::vec::Vec<u8> {
+            match self {
+                Self::ECDSAInvalidSignature(element) => {
+                    ::ethers::core::abi::AbiEncode::encode(element)
+                }
+                Self::ECDSAInvalidSignatureLength(element) => {
+                    ::ethers::core::abi::AbiEncode::encode(element)
+                }
+                Self::ECDSAInvalidSignatureS(element) => {
+                    ::ethers::core::abi::AbiEncode::encode(element)
+                }
+                Self::RevertString(s) => ::ethers::core::abi::AbiEncode::encode(s),
+            }
+        }
+    }
+    impl ::ethers::contract::ContractRevert for ECDSAErrors {
+        fn valid_selector(selector: [u8; 4]) -> bool {
+            match selector {
+                [0x08, 0xc3, 0x79, 0xa0] => true,
+                _ if selector
+                    == <ECDSAInvalidSignature as ::ethers::contract::EthError>::selector() => {
+                    true
+                }
+                _ if selector
+                    == <ECDSAInvalidSignatureLength as ::ethers::contract::EthError>::selector() => {
+                    true
+                }
+                _ if selector
+                    == <ECDSAInvalidSignatureS as ::ethers::contract::EthError>::selector() => {
+                    true
+                }
+                _ => false,
+            }
+        }
+    }
+    impl ::core::fmt::Display for ECDSAErrors {
+        fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
+            match self {
+                Self::ECDSAInvalidSignature(element) => {
+                    ::core::fmt::Display::fmt(element, f)
+                }
+                Self::ECDSAInvalidSignatureLength(element) => {
+                    ::core::fmt::Display::fmt(element, f)
+                }
+                Self::ECDSAInvalidSignatureS(element) => {
+                    ::core::fmt::Display::fmt(element, f)
+                }
+                Self::RevertString(s) => ::core::fmt::Display::fmt(s, f),
+            }
+        }
+    }
+    impl ::core::convert::From<::std::string::String> for ECDSAErrors {
+        fn from(value: String) -> Self {
+            Self::RevertString(value)
+        }
+    }
+    impl ::core::convert::From<ECDSAInvalidSignature> for ECDSAErrors {
+        fn from(value: ECDSAInvalidSignature) -> Self {
+            Self::ECDSAInvalidSignature(value)
+        }
+    }
+    impl ::core::convert::From<ECDSAInvalidSignatureLength> for ECDSAErrors {
+        fn from(value: ECDSAInvalidSignatureLength) -> Self {
+            Self::ECDSAInvalidSignatureLength(value)
+        }
+    }
+    impl ::core::convert::From<ECDSAInvalidSignatureS> for ECDSAErrors {
+        fn from(value: ECDSAInvalidSignatureS) -> Self {
+            Self::ECDSAInvalidSignatureS(value)
         }
     }
 }
