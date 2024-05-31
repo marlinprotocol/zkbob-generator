7cf0dd7189542d0f38973c576310f12fb14432a9
Akshay Meher <akshay_111meher@hotmail.com>
2024-05-31 13:24:46 +0400

    Add Internal IVS inside enclave itself

---
+++ tools/ivs.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Ivs pending");
+    // implement POST /checkInput { input:bytes, secret: bytes } -> returns http_response 200/400
+    // implement POST /checkEncryptedInput { input: bytes, encryptedSecret: bytes } -> returns http_response 200/400
+}
