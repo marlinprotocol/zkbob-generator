870cd47dbfc4f981365096b71a1609d3f55fb0bb
Aniket Baliyan <aniketbaliyan@Anikets-MacBook-Air.local>
2024-06-03 14:48:12 +0530

    test

--- tools/ivs.rs
+++ tools/ivs.rs
@@ -4,6 +4,17 @@ use actix_web::{App, HttpResponse, HttpServer};
 use serde::{Deserialize, Serialize};
 use std::time::Duration;
 
+use ethers::core::k256::ecdsa::{SigningKey, VerifyingKey};
+use ethers::core::rand::thread_rng;
+use ethers::core::utils::hex::FromHex;
+use ethers::signers::{LocalWallet, Signer};
+use ethers::types::{Signature, SignatureError, H160};
+use ethers::utils::keccak256;
+use hex::decode;
+use hex::encode;
+use reqwest::Client;
+use std::error::Error;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Ivs pending");
@@ -78,8 +89,8 @@ pub struct EncryptedInputPayload {
 #[post("/checkEncryptedInputs")]
 async fn check_encrypted_input_handler(
     payload: web::Json<EncryptedInputPayload>,
-) -> impl Responder {
-    #[derive(Deserialize)]
+) -> actix_web::Result<HttpResponse> {
+    #[derive(Debug, Serialize, Deserialize)]
     pub struct DecryptRequest {
         market_id: String,
         private_input: String,
@@ -87,10 +98,88 @@ async fn check_encrypted_input_handler(
         signature: String,
         ivs_pubkey: String,
     }
+
+    let market_id = "1";
+    let signature = sign_message_hash(market_id.to_string());
+    let ivs_pubkey = "04dea308a0d2b8f80f2304818df0eaea9da11d082e58c5018e04d37662b7db2b051d9475505986ed42d099cecbe6f5e5d824ee272d979fae9803190340420dd399";
+
+    let decrypt_request = DecryptRequest {
+        market_id: market_id.to_string(),
+        private_input: payload.clone().encrypted_secrets,
+        acl: payload.clone().public,
+        signature: signature,
+        ivs_pubkey: ivs_pubkey.to_string(),
+    };
+
+    // Create a reqwest Client
+    let client = Client::new();
+
+    // Define the URL for the request
+    let url = "http://localhost:3000/decryptRequest";
     // send request to matching engine `/decryptRequest from here`
-    return zkbob_generator::response::response(
-        "not implemented",
-        StatusCode::NOT_IMPLEMENTED,
+
+    let response = client.post(url).json(&decrypt_request).send().await;
+
+    if response.is_err() {
+        return Ok(zkbob_generator::response::response(
+            "Bad Request",
+            StatusCode::BAD_REQUEST,
+            None,
+        ));
+    }
+
+    return Ok(zkbob_generator::response::response(
+        "Valid Inputs",
+        StatusCode::OK,
         None,
-    );
+    ));
+}
+
+pub fn generate_key() {
+    // Generate a random signing key (private key)
+    let signing_key = SigningKey::random(&mut thread_rng());
+
+    // Derive the verifying key (public key) from the signing key (private key)
+    let verifying_key = VerifyingKey::from(&signing_key);
+
+    // Print the private key in hexadecimal format
+    let private_key_bytes = signing_key.to_bytes();
+    println!("Private Key: 0x{}", encode(private_key_bytes));
+
+    // Print the public key in uncompressed format
+    let binding = verifying_key.to_encoded_point(false);
+    let public_key_bytes = binding.as_bytes();
+    println!("Public Key: 0x{}", encode(public_key_bytes));
+}
+
+pub fn sign_message_hash(market_id: String) -> String {
+    // Generate a random wallet (private key)
+    let private_key_hex = "890d9d749c8cec9babae73e6056ce54e9e96d0f3fc88afa93546a41813ba5ce0";
+
+    // Initialize the wallet from the private key
+    let wallet: LocalWallet = private_key_hex.parse().expect("Invalid private key");
+
+    let ivs_pubkey = "04dea308a0d2b8f80f2304818df0eaea9da11d082e58c5018e04d37662b7db2b051d9475505986ed42d099cecbe6f5e5d824ee272d979fae9803190340420dd399";
+    // Example message
+    let message = market_id;
+
+    // Create a Keccak-256 hash of the message
+    let message_hash = keccak256(&message);
+
+    let signature: Signature = wallet
+        .sign_hash(ethers::types::TxHash(message_hash))
+        .unwrap();
+
+    signature_to_str(signature)
+    // Concatenate the hexadecimal strings
+    // let signature_hex = format!("{}{}{}", r_hex, s_hex, v_hex);
+}
+
+pub fn signature_to_str(signature: Signature) -> String {
+    let r_hex = <[u8; 32]>::try_from(signature.r).unwrap();
+    let s_hex = <[u8; 32]>::try_from(signature.s).unwrap();
+    let v_hex = signature.v.to_string();
+
+    let signature_hex = format!("{}{}{}", hex::encode(r_hex), hex::encode(s_hex), v_hex);
+    signature_hex
 }
